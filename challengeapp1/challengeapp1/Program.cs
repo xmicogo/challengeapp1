ec9b2140977c44eb7107fcb0c7047d7120326626
michal <michalogo@wp.pl>
2024-03-17 22:15:20 +0100

    kod dzien 4

--- challengeapp1/challengeapp1/Program.cs
+++ challengeapp1/challengeapp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = "kobieta";
+var age = 35;
+
+if (sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex == "mężczyzna" && age < 18)
+{
+    Console.WriteLine("niepełnoletni mężczyzna");
+}
