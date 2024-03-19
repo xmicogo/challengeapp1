e3cb2b239243f58ae850f53e8ea6d54c6f16da54
michal <michalogo@wp.pl>
2024-03-19 23:11:39 +0100

    zadanie dzien 5

--- challengeapp1/challengeapp1/Program.cs
+++ challengeapp1/challengeapp1/Program.cs
@@ -1,16 +1,74 @@
-﻿var name = "Ewa";
-var sex = "kobieta";
-var age = 35;
+﻿int number = 123135;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
 
-if (sex == "kobieta" && age < 30)
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
+   
 }
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa lat 30");
-}
-else if (sex == "mężczyzna" && age < 18)
-{
-    Console.WriteLine("niepełnoletni mężczyzna");
-}
+Console.WriteLine("Ile razy występują dane liczby");
+Console.WriteLine();
+
+
+Console.WriteLine("0 -> " + counter0);
+Console.WriteLine("1 -> " + counter1);
+Console.WriteLine("2 -> " + counter2);
+Console.WriteLine("3 -> " + counter3);
+Console.WriteLine("4 -> " + counter4);
+Console.WriteLine("5 -> " + counter5);
+Console.WriteLine("6 -> " + counter6);
+Console.WriteLine("7 -> " + counter7);
+Console.WriteLine("8 -> " + counter8);
+Console.WriteLine("9 -> " + counter9);
